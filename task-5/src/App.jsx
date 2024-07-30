86eafa571e9c9a1b6e1f661b73415cdca64a6e7b
adhsandb05 <adhikarisandeeb@gmail.com>
2024-07-30 16:07:36 -0400

    profile-pic change

--- task-5/src/App.jsx
+++ task-5/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Header from "./components/Header.jsx";
 import Profile from "./components/Profile.jsx";
 import Footer from "./components/Footer.jsx";
-import Button from "./components/Button.jsx";
 
 function App() {
   return (
--- task-5/src/components/Profile.jsx
+++ task-5/src/components/Profile.jsx
@@ -1,18 +1,18 @@
 import ProfileImg from "../assets/profile-pic.png";
 import Button from "./Button";
-import ResumePDF from "../assets/Sandeeb_Resume.pdf";
+//import ResumePDF from "../assets/Sandeeb_Resume.pdf";
 
 function Profile() {
   return (
     <div className="profile">
       <div className="profile-pic">
-        <img src={ProfileImg} alt="Profile" />
+        <img src={ProfileImg} alt="Profile" className="profile-image" />
       </div>
       <div className="profile-info">
         <h2 className="name">Sandeeb Adhikari</h2>
         <p className="text_p2">Web Development</p>
+        <Button />
       </div>
-      <Button />
     </div>
   );
 }
