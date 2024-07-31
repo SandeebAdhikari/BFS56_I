1d1b5d6e73168c2f25d45549d8156d3f3b335c70
adhsandb05 <adhikarisandeeb@gmail.com>
2024-07-31 13:15:25 -0400

    add task-7 folder

---
+++ task-7/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <div>Home</div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
