cbb60835c753f6630b4a6eb12f4e916fa25b0f8e
adhsandb05 <adhikarisandeeb@gmail.com>
2024-08-01 13:23:09 -0400

    change on about.jsx and contact.jsx

--- task-8/src/components/about.jsx
+++ task-8/src/components/about.jsx
@@ -1,20 +1,47 @@
 function about() {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row">
+    <div className="carousel glass rounded-box w-96">
+      <div className="carousel-item w-1/2">
         <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
+          src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+          className="w-full"
         />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
-        </div>
+      </div>
+      <div className="carousel-item w-1/2">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+          className="w-full"
+        />
+      </div>
+      <div className="carousel-item w-1/2">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+          className="w-full"
+        />
+      </div>
+      <div className="carousel-item w-1/2">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+          className="w-full"
+        />
+      </div>
+      <div className="carousel-item w-1/2">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
+          className="w-full"
+        />
+      </div>
+      <div className="carousel-item w-1/2">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+          className="w-full"
+        />
+      </div>
+      <div className="carousel-item w-1/2">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+          className="w-full"
+        />
       </div>
     </div>
   );
--- task-8/src/components/contact.jsx
+++ task-8/src/components/contact.jsx
@@ -1,5 +1,27 @@
 function contact() {
-  return <div>contact</div>;
+  return (
+    <div>
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-3xl font-bold">Contact Us</h1>
+        <div className="flex flex-col items-start gap-1">
+          <label className="text-lg ">Full Name</label>
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="rounded-lg border border-gray-300 p-2 w-full "
+          />
+        </div>
+        <div className="flex flex-col items-start gap-1">
+          <label className="text-lg">Email</label>
+          <input
+            type="email"
+            placeholder="Search"
+            className="form-input px-4 py-3 rounded-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default contact;
